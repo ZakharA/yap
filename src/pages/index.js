13df6a230609122047351afaf5664f6775863385
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-09-14 09:37:50 +1000

    remove unused Img component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
 import Layout from "../components/layout"
 import Biography from "../components/content/biography"
 import SEO from "../components/seo"
