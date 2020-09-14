b775af4eeffb37a8eaaca29c7db4d72c46821349
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-09-14 12:34:07 +1000

    on project click redirect to a corresponding github page

--- src/components/content/repository.js
+++ src/components/content/repository.js
@@ -4,7 +4,14 @@ import { Card, CardBody, CardFooter, Paragraph, Heading } from "grommet"
 import RepoTopics from "./repoTopics"
 const Repository = ({ repository, index }) => {
   return (
-    <Card pad="medium" gap="small" key={index}>
+    <Card
+      pad="medium"
+      gap="small"
+      key={index}
+      onClick={() => {
+        window.location.href = repository.node.url
+      }}
+    >
       <CardBody>
         <Heading level="3">{repository.node.name}</Heading>
         <Paragraph size="small" fill={true}>
