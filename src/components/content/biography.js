ad5c2d84638fa43c9c0ce76ce505b72ef10cc81f
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-09-15 15:46:45 +1000

    refactor bio content

--- src/components/content/biography.js
+++ src/components/content/biography.js
@@ -1,8 +1,9 @@
 import React from "react"
 
-import { Grid, Box, ResponsiveContext } from "grommet"
+import { Box } from "grommet"
 import ProfileContent from "./profileContent"
 import BioContent from "./bioContent"
+import ResponsiveGrid from "./responvsiveGrid"
 
 const columns = {
   small: ["auto"],
@@ -37,53 +38,13 @@ const fixedGridAreas = {
   ],
 }
 
-const ResponsiveGrid = ({
-  children,
-  overrideColumns,
-  overrideRows,
-  areas,
-  ...props
-}) => (
-  <ResponsiveContext.Consumer>
-    {size => {
-      // Take into consideration if not array is sent but a simple string
-      let columnsVal = columns
-      if (columns) {
-        if (columns[size]) {
-          columnsVal = columns[size]
-        }
-      }
-
-      let rowsVal = rows
-      if (rows) {
-        if (rows[size]) {
-          rowsVal = rows[size]
-        }
-      }
-
-      let areasVal = areas
-      if (areas && !Array.isArray(areas)) areasVal = areas[size]
-
-      return (
-        <Grid
-          areas={!areasVal ? undefined : areasVal}
-          rows={!rowsVal ? size : rowsVal}
-          columns={!columnsVal ? size : columnsVal}
-        >
-          {children}
-        </Grid>
-      )
-    }}
-  </ResponsiveContext.Consumer>
-)
-
 const Biography = ({ image }) => {
   return (
     <ResponsiveGrid
       rows={rows}
       columns={columns}
-      gap="small"
       areas={fixedGridAreas}
+      gap="small"
       margin="medium"
       pad="large"
     >
